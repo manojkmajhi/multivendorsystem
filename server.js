cac716bf2b913ca1c27198dfe4aced21f15a224c
gauravkrsah <gauravkumarsah.078@kathford.edu.np>
2025-10-14 17:36:01 +0545

    imporoved area slector and payment deseign

--- server.js
+++ server.js
@@ -64,8 +64,8 @@ async function setSetting(key, value){
 }
 
 app.use(cookieParser());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 // Performance and security headers
 app.use((req,res,next)=>{
